fad6dfe12fe258b824d2b6ce0f527951d2dbf606
LoiTran <tranloi781@gmail.com>
2022-08-04 08:17:02 -0500

    lesson 14. 2 full

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
-
 router.get('/', (req, res) => {
+  console.log(req.session);
     Post.findAll({
       attributes: [
         'id',
@@ -38,4 +38,16 @@ router.get('/', (req, res) => {
       });
   });
 
+  router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+
+  router.post('/logout', (req, res) => {
+
+  });
 module.exports = router;
--- server.js
+++ server.js
@@ -3,9 +3,22 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+
+
 const hbs = exphbs.create({});
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,6 +26,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
