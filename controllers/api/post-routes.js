5e29c82f2abeaf2e2c250c5d87ed40fd56ee150c
LoiTran <tranloi781@gmail.com>
2022-08-04 07:38:00 -0500

    lesson 14.1.6

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -4,40 +4,33 @@ const { Post, User, Vote, Comment } = require("../../models");
 
 
 // get all users
-router.get("/", (req, res) => {
-  console.log("======================");
+router.get('/', (req, res) => {
+  console.log('======================');
   Post.findAll({
-    order: [["created_at", "DESC"]],
     attributes: [
-      "id",
-      "post_url",
-      "title",
-      "created_at",
-      [
-        sequelize.literal(
-          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
-        ),
-        "vote_count",
-      ],
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     include: [
-      // include the Comment model here:
       {
         model: Comment,
-        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
         include: {
           model: User,
-          attributes: ["username"],
-        },
+          attributes: ['username']
+        }
       },
       {
         model: User,
-        attributes: ["username"],
-      },
-    ],
+        attributes: ['username']
+      }
+    ]
   })
-    .then((dbPostData) => res.json(dbPostData))
-    .catch((err) => {
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
